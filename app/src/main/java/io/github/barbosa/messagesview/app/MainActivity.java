84167c032fc409beecd2ebab120e9bd22cdadaab
Gustavo Barbosa <gustavocsb@gmail.com>
2015-08-10 16:52:14 -0400

    app: lorem ipsum texts

--- app/src/main/java/io/github/barbosa/messagesview/app/MainActivity.java
+++ app/src/main/java/io/github/barbosa/messagesview/app/MainActivity.java
@@ -20,6 +20,19 @@ public class MainActivity extends AppCompatActivity {
     private static final String MONSTER_3_AVATAR_URL = "https://cloud.githubusercontent.com/assets/235208/9158124/49a0fff2-3edf-11e5-832d-24cff752d049.png";
     private static final String MONSTER_4_AVATAR_URL = "https://cloud.githubusercontent.com/assets/235208/9158145/94dd9188-3edf-11e5-9ba5-1420b706cffc.png";
 
+    private static final String[] TEXTS = new String[] {
+            "I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.",
+            "Science has not yet mastered prophecy.",
+            "The Earth was small, light blue, and so touchingly alone, our home that must be defended like a holy relic.",
+            "The Earth was absolutely round. I believe I never knew what the word round meant until I saw Earth from space.",
+            "Agree.",
+            "Dinosaurs are extinct today because they lacked opposable thumbs and the brainpower to build a space program.",
+            "Here men from the planet Earth first set foot upon the Moon.",
+            "To be the first to enter the cosmos, to engage, single-handed, in an unprecedented duel with nature—could one dream of anything more?",
+            "You have to get to know the meaning not just of joy but also of grief, before being allowed in the spacecraft cabin.",
+            "Astronomy compels the soul to look upward, and leads us from this world to another."
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +49,15 @@ public class MainActivity extends AppCompatActivity {
     private List<Message> getMessages() {
         List<Message> messages = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            Sender sender = new Sender("Gustavo", "Barbosa", MONSTER_1_AVATAR_URL, i % 2 == 0);
+            boolean isMe = i == 2 || i == 6;
+            Sender sender = new Sender("Gustavo", "Barbosa", isMe ? MONSTER_4_AVATAR_URL : MONSTER_3_AVATAR_URL, isMe);
             Message message = new Message(sender);
-            message.setText("Hello " + (i + 1));
+            message.setText(TEXTS[i]);
 
             messages.add(message);
         }
 
         return messages;
     }
+
 }
