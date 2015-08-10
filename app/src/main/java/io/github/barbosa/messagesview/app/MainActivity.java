8ef52a09cc0a5ea166b4722871e6509153e80daa
Gustavo Barbosa <gustavocsb@gmail.com>
2015-08-09 21:56:04 -0400

    style: theming

--- app/src/main/java/io/github/barbosa/messagesview/app/MainActivity.java
+++ app/src/main/java/io/github/barbosa/messagesview/app/MainActivity.java
@@ -15,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
     MessagesView messagesView;
 
+    private static final String MONSTER_1_AVATAR_URL = "https://cloud.githubusercontent.com/assets/235208/9158151/a87d0ad4-3edf-11e5-98a1-6dada6f8f242.png";
+    private static final String MONSTER_2_AVATAR_URL = "https://cloud.githubusercontent.com/assets/235208/9158161/b76e3810-3edf-11e5-8bea-9e2d4c7d2474.png";
+    private static final String MONSTER_3_AVATAR_URL = "https://cloud.githubusercontent.com/assets/235208/9158124/49a0fff2-3edf-11e5-832d-24cff752d049.png";
+    private static final String MONSTER_4_AVATAR_URL = "https://cloud.githubusercontent.com/assets/235208/9158145/94dd9188-3edf-11e5-9ba5-1420b706cffc.png";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private List<Message> getMessages() {
         List<Message> messages = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            Sender sender = new Sender("Gustavo", "Barbosa", "http://pickaface.net/includes/themes/clean/img/slide2.png", i % 2 == 0);
+            Sender sender = new Sender("Gustavo", "Barbosa", MONSTER_1_AVATAR_URL, i % 2 == 0);
             Message message = new Message(sender);
             message.setText("Hello " + (i + 1));
 
